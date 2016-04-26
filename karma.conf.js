3fd6eab5ed8a6190ef4d2b0b08f249ec0afe0a9c
nurmjjou <jouni.nurmi@tieto.com>
2016-04-26 13:53:47 +0300

    junit

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,8 @@ module.exports = function(config) {
       'karma-mocha-reporter',
       'karma-chrome-launcher',
       'karma-riot',
-      'karma-sinon-chai'
+      'karma-sinon-chai',
+      'karma-junit-reporter'
     ],
 
 
@@ -43,8 +44,11 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
+    reporters: ['mocha', 'junit'],
 
+    junitReporter: {
+      outputDir: 'tests/unit/results'
+    },
 
     // web server port
     port: 9876,
